c38b5dc4413307f1a7cb8f66c8c95de159094dd6
PavelShadrov <44029161+PavelShadrov@users.noreply.github.com>
2020-11-17 13:19:04 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,185 @@
+/*=========================================
+
+Template Name: Rockstar Template
+Author: www.godexdesign.com
+Version: 1.0
+Design and Developed by: Godex Design
+
+NOTE: This is the custom jQuery file for the template
+
+=========================================*/
+
+
+(function ($) {
+	"use strict";
+    
+    var $window = $(window),
+            $body = $('body');
+    
+    jQuery(document).ready(function($){
+
+
+        /*======== Sticky header ===========*/
+        $('.navbar-collapse a').on('click',function(){
+          $(".navbar-collapse").collapse('hide');
+        });
+
+        $window.on('scroll', function() {
+          if ($(".navbar").offset().top > 100) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+              } else {
+                $(".navbar-fixed-top").removeClass("top-nav-collapse");
+              }
+        });
+      
+        /*======== Smoothscroll js ===========*/
+        $(function() {
+          $('a.smoth-scroll').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1000);
+            event.preventDefault();
+          });
+        });  
+            
+        /*======== jquery scroll spy ===========*/
+        $body.scrollspy({
+            target : ".navbar-collapse",
+            offset : 95
+        });
+        
+        /*========  Bootstrap menu fix ===========*/
+        $(".navbar-toggle").on("click", function(){
+            $body.addClass("mobile-menu-activated");
+        });
+        
+        $("ul.nav.navbar-nav li a").on("click", function(){
+            $(".navbar-collapse").removeClass("in");
+        });
+        
+        /*======== background-image flickering solution for mobile ===========*/
+        var bg = jQuery("#home");
+        function resizeBackground() {
+            bg.height(jQuery(window).height() + 60);
+        }
+      resizeBackground();
+        
+        /*======== Modal js ===========*/
+        $("#projectModal1, #projectModal2, #projectModal3, #projectModal4, #projectModal5, #projectModal6, #projectModal7, #projectModal8, #projectModal9").on('hidden.bs.modal', function (e) {
+            $("#projectModal1 iframe, #projectModal2 iframe, #projectModal3 iframe, #projectModal3 iframe, #projectModal4 iframe, #projectModal5 iframe, #projectModal6 iframe, #projectModal7 iframe, #projectModal8 iframe, #projectModal9 iframe").attr("src", $("projectModal1 iframe, #projectModal2 iframe, #projectModal3 iframe, #projectModal3 iframe, #projectModal4 iframe, #projectModal5 iframe, #projectModal6 iframe, #projectModal7 iframe, #projectModal8 iframe, #projectModal9 iframe").attr("src"));
+        });
+   
+        /*======== Intro typer ===========*/
+        var element = $(".element");
+        $(function() {
+            element.typed({
+                strings: ["Professional Server.", "Friendly community.", "Daily events."],
+                typeSpeed: 100,
+                loop: true,
+            });
+        });
+        
+        /*======== Parallax js ===========*/
+        $window.stellar({
+            responsive: true,
+            positionProperty: 'position',
+            horizontalScrolling: false
+        });
+        
+        /*======== Magnific Popup ===========*/
+        $('.work-popup').magnificPopup({type:'image'});
+        
+         /*========  CounterUp ===========*/
+        $('.counter').counterUp({
+            delay: 4,
+             time: 800
+        });
+ 
+        /*========   WOW js===========*/
+        new WOW({ mobile: false }).init();
+    
+        /*======== jQuery mixItUp ===========*/
+        $('.work-inner').mixItUp();
+    
+        /*========   owl carousel testimonial ===========*/
+        $(".testimonial-list").owlCarousel({
+            loop:true,
+            margin:30,
+            nav:false,
+            dots:true,
+            autoplay:true,
+            autoplayHoverPause:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:2
+                },
+                1000:{
+                    items:3
+                }
+            }
+        });
+    
+
+    });
+    
+   
+    $window.on('load', function(){
+        
+        /*======== Preloder ===========*/
+       setTimeout(function() {
+            $('body').addClass('loaded');
+        }, 2000);
+
+    });
+    
+    $('a[href=#top]').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 80) {
+        $('.totop a').fadeIn();
+    } else {
+        $('.totop a').fadeOut();
+    }
+});
+
+
+	//material contact form
+	$('.contact-form').find('.form-control').each(function() {
+	  var targetItem = $(this).parent();
+	  if ($(this).val()) {
+		$(targetItem).find('label').css({
+		  'top': '10px',
+		  'fontSize': '14px'
+		});
+	  }
+	})
+	$('.contact-form').find('.form-control').focus(function() {
+	  $(this).parent('.input-block').addClass('focus');
+	  $(this).parent().find('label').animate({
+		'top': '10px',
+		'fontSize': '14px'
+	  }, 300);
+	})
+	$('.contact-form').find('.form-control').blur(function() {
+	  if ($(this).val().length == 0) {
+		$(this).parent('.input-block').removeClass('focus');
+		$(this).parent().find('label').animate({
+		  'top': '25px',
+		  'fontSize': '18px'
+		}, 300);
+	  }
+	})
+
+
+
+}(jQuery));	
+
